c02f57c1ddac3d283fa95cb6c56f4c0035276da1
michael verdi <michael.v.verdi@gmail.com>
2012-06-03 22:46:24 -0400

    fixed ajax form bs

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
   def create
     chat_room = ChatRoom.find(params[:message][:chat_room_id])
-    message = chat_room.messages.new(params[:message])
+    @message = chat_room.messages.new(params[:message])
     respond_to do |format|
-      if message.save
+      if @message.save
         format.js
         format.html { redirect_to chat_room_path(chat_room) }
       else
