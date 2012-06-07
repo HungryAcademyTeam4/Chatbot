f863491ba029d098a025140fdad2338834baa168
michael verdi <michael.v.verdi@gmail.com>
2012-06-07 11:21:26 -0400

    added unicorn and cap gem

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'chatbot_api', git: "git://github.com/HungryAcademyTeam4/chatbot_api.git"
 gem 'faraday'
 gem 'faye'
 gem 'thin'
+gem 'capistrano'
+gem 'unicorn'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -19,7 +22,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+MODE = "staging"
+PRODUCTION_SERVER = "fallingfoundry.com"
+STAGING_SERVER = "2.2.2.2:3000"
+SERVER = MODE == "production" ? PRODUCTION_SERVER : STAGING_SERVER
+
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+ require 'bundler/capistrano'
+ require 'rvm/capistrano'
+server SERVER, :web, :db, :app, primary: true
+set :user, "deployer"
+set :application, "Chatbot"
+
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, true
+
+set :scm, "git"
+set :repository,  "git@github.com:HungryAcademyTeam4/Chatbot.git"
+set :branch, "faye"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+namespace :deploy do
+  task :bundle_install do
+    run "cd /home/#{user}/apps/#{application}/current && bundle install"
+  end
+  task :create_release_log do
+    run <<-CMD
+    rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+    mkdir -p #{latest_release}/public &&
+    mkdir -p #{latest_release}/tmp &&
+    ln -s #{shared_path}/log #{latest_release}/log &&
+    ln -s #{shared_path}/system #{latest_release}/public/system &&
+    ln -s #{shared_path}/pids #{latest_release}/tmp/pids
+    CMD
+  end
+  task :start do
+    run "cd /home/#{user}/apps/#{application}/current && RAILS_ENV=production bundle exec rake db:create && bundle exec rake db:migrate"
+    run "cd /home/#{user}/apps/#{application}/current && RAILS_ENV=production bundle exec puma -p 4568"
+  end
+  after "deploy", "deploy:create_release_log"
+  after "deploy", "deploy:start"
+end
