4b54d508018132fa999b206cfaa077598a47a9bc
michael verdi <michael.v.verdi@gmail.com>
2012-06-11 16:31:27 -0400

    updated url

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message < ActiveRecord::Base
   validates_presence_of :content, :chat_room_id
 
   def broadcast
-    url = "http://localhost:9000/faye"
+    url = "http://fallinggarden.com:9000/faye"
     message = { channel: "/#{self.chat_room_id}", data: { 'text' => self.content } }
     Net::HTTP.post_form(URI.parse(url), message: message.to_json)
   end
