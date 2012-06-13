056fae825276dcf48e58c7910350dd6fa6eaffbe
michael verdi <michael.v.verdi@gmail.com>
2012-06-13 15:55:24 -0400

    deleted all channel faye broadcast method

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,26 +1,21 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :chat_room_id, :user_name
   belongs_to :chat_room
-  after_create :broadcast_to_specific_channel, :broadcast_to_combined_channels
+  after_create :broadcast_to_specific_channel
   validates_presence_of :content, :chat_room_id
 
   def contents_hash
-    { 'content'    => self.content,
-      'user_id'    => self.user_id,
-      'created_at' => self.created_at,
-      'id'         => self.id,
-      'user_name'  => self.user_name }
+    { 'content'      => self.content,
+      'user_id'      => self.user_id,
+      'created_at'   => self.created_at,
+      'id'           => self.id,
+      'user_name'    => self.user_name,
+      'chat_room_id' => self.chat_room_id }
   end
 
   def broadcast_to_specific_channel
     url = "http://localhost:9000/faye"
     message = { channel: "/#{self.chat_room_id}", data: contents_hash }
-    Net::HTTP.post_form(URI.parse(url), message: message.to_json)
-  end
-
-  def broadcast_to_combined_channels
-    url = "http://localhost:9000/faye"
-    message = { channel: "/all_messages", data: contents_hash }
     Net::HTTP.post_form(URI.parse(url), message: message.to_json)
   end
 end
